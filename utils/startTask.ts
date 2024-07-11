aa9ce0e17277e352dd17eb4449b47c0ab464837e
1adybug <lurongv@qq.com>
2024-07-12 00:20:45 +0800

    ✨feature: 大量改进

--- utils/startTask.ts
+++ utils/startTask.ts
@@ -1,16 +1,33 @@
-import { join, relative } from "path"
+import { readdir } from "fs/promises"
+import { join, relative, resolve } from "path"
 import { execAsync } from "soda-nodejs"
 import { DIR, NAMESPACE } from "../constants"
 import { deleteTask } from "./deleteTask"
-import { getEnvStr } from "./getEnvStr"
 import { getProject } from "./getProject"
+import { getProjectType, ProjectType } from "./getProjectType"
+import { zipNext } from "./zipNext"
 
 export async function startTask(id: string) {
+    const { env, core, port, current } = await getProject(id)
+    if (!current) throw new Error("未找到当前版本")
     await deleteTask(id)
-    const { env, core, port } = await getProject(id)
-    env.PORT ??= port.toString()
+    const type = await getProjectType(id, current)
+    if (type === "next") await zipNext(id, current)
     const cwd = join(DIR, id, "main")
-    const startPath = join("scripts", "startTask.mjs")
-    console.log(`cross-env${getEnvStr(env)} pm2 start ${relative(cwd, startPath)} --name ${id} --namespace ${NAMESPACE} -i ${core}`)
-    await execAsync(`cross-env${getEnvStr(env)} pm2 start ${relative(cwd, startPath)} --name ${id} --namespace ${NAMESPACE} -i ${core}`, { cwd })
+    let start = ""
+    if (type === ProjectType.next) start = resolve("scripts", "startNext.js")
+    if (type === ProjectType.static) start = resolve("scripts", "startNext.js")
+    if (type === ProjectType.script) {
+        const dir = await readdir(join(DIR, id, "releases", current, "dist"))
+        const script = dir.find(file => file.toLowerCase().endsWith(".js") || file.toLowerCase().endsWith(".mjs") || file.toLowerCase().endsWith(".cjs"))!
+        start = join("dist", script)
+    }
+    await execAsync(`pm2 start ${relative(cwd, start)} --name ${id} --namespace ${NAMESPACE} -i ${core}`, {
+        cwd,
+        env: {
+            NODE_ENV: "production",
+            PORT: port.toString(),
+            ...env
+        }
+    })
 }
