2b7a7f64be7c477c5b08136da6537285f8e65e2c
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-07-15 11:07:45 +0800

    ✨feature: 升级 soda-nodejs 依赖

--- utils/getTaskList.ts
+++ utils/getTaskList.ts
@@ -4,7 +4,7 @@ import { execAsync } from "soda-nodejs"
 import { NAMESPACE } from "../constants"
 
 export async function getTaskList(): Promise<Pm2.Task[]> {
-    let { stdout } = await execAsync(`pm2 jlist`)
+    let stdout = await execAsync(`pm2 jlist`)
     stdout = stdout.replace(/^[\d\D]*?\[/m, "[")
     return (JSON.parse(stdout) as Pm2.Task[]).filter(task => task.pm2_env.namespace === NAMESPACE)
 }
